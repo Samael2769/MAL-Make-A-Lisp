04e25d4a067f1ab0114ef6bb770bc1ee76ebbace
Samael <jeremie.joannard@epitech.eu>
2023-11-06 08:00:31 +0900

    [ADD] sam_ruby/env.rb, [ADD] sam_ruby/errors.rb, [ADD] sam_ruby/printer.rb, [ADD] sam_ruby/reader.rb, [ADD] sam_ruby/step1_read_print.rb, [ADD] sam_ruby/types.rb,
    Adding step1 reader and printer

---
+++ sam_ruby/errors.rb
@@ -0,0 +1,54 @@
+
+    class Error < ::StandardError; end
+    class MyTypeError < ::TypeError; end
+  
+    class MalError < Error
+      attr_reader :value
+  
+      def initialize(value)
+        @value = value
+      end
+  
+      def message
+        value.inspect
+      end
+    end
+  
+    class FileNotFoundError < Error; end
+    class MyIndexError < TypeError; end
+    class SkipCommentError < Error; end
+  
+    class InvalidHashmapKeyError < TypeError; end
+    class InvalidIfExpressionError < TypeError; end
+    class InvalidLetBindingsError < TypeError; end
+    class InvalidReaderPositionError < Error; end
+    class InvalidTypeError < TypeError; end
+  
+    class NotCallableError < Error; end
+  
+    class SymbolNotFoundError < Error; end
+    class MySyntaxError < TypeError; end
+  
+    class UnbalancedEscapingError < Error; end
+    class UnbalancedHashmapError < Error; end
+    class UnbalancedListError < Error; end
+    class UnbalancedStringError < Error; end
+    class UnbalancedVectorError < Error; end
+  
+    class UnknownError < Error
+      attr_reader :original_error
+  
+      def initialize(original_error)
+        @original_error = original_error
+      end
+  
+      def inspect
+        "UnknownError :: #{original_error.inspect}"
+      end
+  
+      def message
+        "UnknownError<#{original_error.class}> :: #{original_error.message}"
+      end
+    end
+
+  
